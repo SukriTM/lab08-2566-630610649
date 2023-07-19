c05a756c1b9d688bf9341aaa910342618e57d8dd
SukriTM <micsu.ky@gmail.com>
2023-07-19 17:43:02 +0700

    08done

---
+++ components/TaskInput.jsx
@@ -0,0 +1,10 @@
+function TaskInput() {
+    return (
+      <div className="d-flex gap-1">
+        <input className="form-control" placeholder="Insert a task here.." />
+        <button className="btn btn-primary">Add</button>
+      </div>
+    );
+  }
+  
+  export default TaskInput;
